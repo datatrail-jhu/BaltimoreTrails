f5e23ab04c912569a5496f983b7970c3d3cea99d
Franklin Fuchs <fuchs.franklin@gmail.com>
2023-03-14 21:51:09 -0400

    Adding data adding workflow to inst

---
+++ inst/data-adding/Workflow_Prelim.R
@@ -0,0 +1,56 @@
+####### Workflow For Adding Data to the Package #######
+
+library(devtools)
+library(roxygen2)
+
+
+####### EDUCATION #######
+# Adding Raw EDUCATION Data 1
+use_data_raw("Perc_Pop_School_Employed")
+
+# Adding Raw EDUCATION Data 2
+use_data_raw("High_School_Completion_Rate")
+
+# Adding Raw EDUCATION Data 3
+use_data_raw("Kindergarten_Readiness")
+
+# Adding Raw EDUCATION Data 4
+use_data_raw("Percentage_of_Students_Receiving_Free_or_Reduced_Meals")
+
+# Adding Raw EDUCATION Data 5
+use_data_raw("Percentage_of_Students_Suspended_or_Expelled_During_School_Year")
+
+
+####### SUSTAINABILITY #######
+# Adding Raw SUSTAINABILITY Data 1
+use_data_raw("Percent_of_Households_with_No_Vehicles_Available")
+
+# Adding Raw SUSTAINABILITY Data 2
+use_data_raw("Percent_of_Employed_Population_with_Travel_Time_to_Work_of_45_Minutes_and_Over")
+
+# Adding Raw SUSTAINABILITY Data 3
+use_data_raw("Percent_of_Population_that_Uses_Public_Transportation_to_Get_to_Work")
+
+# Adding Raw SUSTAINABILITY Data 4
+use_data_raw("Percent_of_Population_that_Carpool_to_Work")
+
+# Adding Raw SUSTAINABILITY Data 5
+use_data_raw("Percent_of_Population_that_Walks_to_Work")
+
+
+####### Children and Family Health #######
+# Adding Raw Health Data 1
+use_data_raw("Fast_Food_Outlet_Density_per_1000_Residents")
+
+# Adding Raw Health Data 2
+use_data_raw("Percent_of_Children_with_Elevated_Blood_Lead_Levels")
+
+# Adding Raw Health Data 3
+use_data_raw("Life_Expectancy")
+
+# Adding Raw Health Data 4
+use_data_raw("Infant_Mortality_Rate")
+
+# Adding Raw Health Data 5
+use_data_raw("Teen_Birth_Rate_per_1000_Females")
+
